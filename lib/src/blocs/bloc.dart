9eda229c6e256277c2101c874aabbdbacb5c2f97
Alexis Villegas Torres <alexisvt@gmail.com>
2019-02-18 15:07:46 -0600

    finishing sample

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -4,8 +4,8 @@ import './validators.dart';
 import 'package:rxdart/rxdart.dart';
 
 class Bloc with Validators {
-  final _email = StreamController<String>.broadcast();
-  final _password = StreamController<String>.broadcast();
+  final _email = BehaviorSubject<String>();
+  final _password = BehaviorSubject<String>();
 
 // Add data to stream
   Stream<String> get email => _email.stream.transform(validateEmail);
@@ -16,6 +16,14 @@ class Bloc with Validators {
 // Change data
   Function(String) get changeEmail => _email.sink.add;
   Function(String) get changePassword => _password.sink.add;
+
+  void submit() {
+    final validEmail = _email.value;
+    final validPassword = _password.value;
+
+    print('Email is $validEmail');
+    print('Password is $validPassword');
+  }
 
   void dispose() {
     _email.close();
